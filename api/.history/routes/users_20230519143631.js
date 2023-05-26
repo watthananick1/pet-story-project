f5e2eef360944c65d2eb72b9d747bd89d3a651c0
nick <86664451+watthananick1@users.noreply.github.com>
2023-05-26 21:07:48 +0700

    Update firebaseConfig v1

--- api/.history/routes/users_20230519143631.js
+++ api/.history/routes/users_20230519143631.js
@@ -6,13 +6,13 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyCFUBWxesLk-BX8KwwQfaI8Gs3cUCcBVWA',
+  authDomain: 'pet-story-f51e3.firebaseapp.com',
+  databaseURL: 'https://pet-story-f51e3-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'pet-story-f51e3',
+  storageBucket: 'pet-story-f51e3.appspot.com',
+  messagingSenderId: '576128138584',
+  appId: '1:576128138584:web:c52f7384461830b1eeb92d',
 };
 
 firebase.initializeApp(firebaseConfig);
