1a9bde263b4f782e9089914d5e5628bfc51260f2
nick <86664451+watthananick1@users.noreply.github.com>
2023-06-05 06:08:43 +0700

    Update All V3

---
+++ client/.history/src/apiCalls_20230605031938.js
@@ -0,0 +1,32 @@
+import axios from "axios"
+
+export const loginCall = async (userCredential, dispatch) => {
+  dispatch({ type: "LOGIN_START" });
+
+  try {
+    const res = await axios.post("/api/auth/Login", userCredential);
+    const user = res?.data.user;
+    const data = user?.user.user.uid
+    
+    console.log('User=',data)
+    fetchUserData(data, dispatch)
+  } catch (err) {
+    // Dispatch LOGIN_FAILURE action with the error
+    dispatch({ type: "LOGIN_FAILURE", payload: err.response.data.error });
+  }
+};
+
+export const fetchUserData = async (userId, dispatch) => {
+  try {
+    console.log("Fetching user".)
+    const res = await axios.get(`/api/users/GETuser/${userId}`);
+    const userData = res.data;
+    console.log('User=',userData)
+    // Dispatch action to update user data in the state
+    dispatch({ type: "LOGIN_SUCCESS", payload: userData });
+  } catch (err) {
+    // Handle error
+    console.error("Failed to fetch user data:", err);
+  }
+};
+
