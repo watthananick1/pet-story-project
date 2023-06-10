6d2fb8ad609cfaa3615de8994e96adf53b79781f
nick <86664451+watthananick1@users.noreply.github.com>
2023-06-10 19:42:31 +0700

    Update  All V3

---
+++ client/.history/src/components/feed/Feed_20230609134848.jsx
@@ -0,0 +1,101 @@
+import { useContext, useEffect, useState } from "react";
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./feed.css";
+import axios from "axios";
+import { AuthContext } from "../../context/AuthContext";
+import ReactLoading from "react-loading";
+import io from "socket.io-client";
+
+export default function Feed({ firstName, onProfile }) {
+  const [posts, setPosts] = useState([]);
+  const { user } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    const socket = io.connect(process.env.PATH_ID); // Fix the environment variable name
+
+    const fetchPosts = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get(`/api/posts/${user.member_id}/date`, {
+          cancelToken: source.token,
+        });
+        const sortedPosts = res.data.sort((a, b) => {
+          return new Date(b.createdAt) - new Date(a.createdAt);
+        });
+        setPosts(sortedPosts);
+      } catch (err) {
+        if (axios.isCancel(err)) {
+          console.log("Request canceled:", err.message);
+        } else {
+          console.log(err);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const fetchUserPosts = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get(`/api/posts/user/${firstName}/date`);
+        // const sortedPosts = res.data.sort((a, b) => {
+        //   const date1 = new Date(a.createdAt);
+        //   const date2 = new Date(b.createdAt);
+        //   return date2.getTime() - date1.getTime();
+        // });
+        // setPosts(sortedPosts);
+        
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const handleNewPost = (newPost) => {
+      setPosts((prevPosts) => [newPost, ...prevPosts]);
+    };
+
+    if (onProfile) {
+      fetchUserPosts();
+    } else {
+      fetchPosts();
+    }
+
+    socket.on("newPost", handleNewPost);
+
+    return () => {
+      source.cancel("Component unmounted");
+      console.log("Component unmounted", source);
+      socket.off("newPost", handleNewPost);
+      socket.disconnect();
+    };
+  }, [onProfile, firstName, user.member_id]);
+
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        {!firstName || firstName === user?.firstName ? <Share /> : null}
+        {loading ? (
+          <div className="loadingWrapper">
+            <ReactLoading
+              type="spin"
+              color="#6200E8"
+              height={"10%"}
+              width={"10%"}
+            />
+          </div>
+        ) : (
+          <>
+            {posts.map((p, i) => (
+              <Post key={i} post={p} indexPost={i} />
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ client/.history/src/components/post/Post_20230610143916.jsx
@@ -0,0 +1,577 @@
+import "./post.css";
+import { useState, useEffect, useContext, useRef } from "react";
+import axios from "axios";
+import { format } from "timeago.js";
+import { Link } from "react-router-dom";
+import { NestedModal, ReportModal } from "../modelEdit/ModalEdit";
+import io from "socket.io-client";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardActions,
+  Collapse,
+  Typography,
+  IconButton,
+  Avatar,
+  TextField,
+  Button,
+  Chip,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import ReportIcon from "@mui/icons-material/Report";
+import {
+  MoreVert,
+  FavoriteBorder,
+  Favorite,
+  Comment,
+  MoreHoriz,
+} from "@mui/icons-material";
+import { AuthContext } from "../../context/AuthContext";
+import { SortableContainer, SortableElement } from "react-sortable-hoc";
+// import ReactPlayer from 'react-player';
+import ReactPlayer from "react-player/lazy";
+import ReactLoading from "react-loading";
+
+export default function Post({ isPost, onPostUpdate, indexPost }) {
+  const { user } = useContext(AuthContext);
+  const [post, setPost] = useState(isPost);
+  const [like, setLike] = useState(post.likes.length);
+  const [isLiked, setIsLiked] = useState(false);
+  const [comments, setComments] = useState([]);
+  const [commentsData, setCommentsData] = useState([]);
+  const [showAllComments, setShowAllComments] = useState(false);
+  const [newCommentText, setNewCommentText] = useState("");
+  const [showComments, setShowComments] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorElComment, setAnchorElComment] = useState(false);
+  const [commentIdToDelete, setCommentIdToDelete] = useState(null);
+  const [commentIdUser, setCommentIdUser] = useState("");
+  const [loadingComment, setLoadingComment] = useState(false);
+  const maxDisplayedComments = 3;
+  const { user: currentUser } = useContext(AuthContext);
+  const [openModal, setOpenModal] = useState(false);
+  const [openReportModal, setOpenReportModal] = useState(false);
+  const [typeModal, setTypeModal] = useState("Post");
+  const containerRef = useRef(null);
+  const [dataEdit, setdataEdit] = useState([]);
+  const [dataPUser, setdataPUser] = useState({});
+  const [dataEditID, setdataEditID] = useState(null);
+  const [isAddComment, setIsAddComment] = useState(false);
+  const createdAt = new Date(post.createdAt.seconds * 1000);
+  const formattedDate = format(createdAt);
+  const socket = io.connect(process.env.PATH_ID);
+
+  //++++++++++++++++++ fetch Data +++++++++++++++++++
+  useEffect(() => {
+    // Set initial isLiked state
+    setIsLiked(post.likes.includes(currentUser.member_id));
+
+    // Listen for newLike events and update isLiked state
+    // Listen for postUpdate event from the server
+    socket.on("postUpdate", (updatedPost) => {
+      // Update the local state variables based on the received data
+      if (updatedPost.id === post.id) {
+        setPost(updatedPost);
+        setIsLiked(updatedPost.likes.includes(currentUser.member_id));
+      }
+    });
+
+    // Cleanup socket connection
+    return () => {
+      socket.off("postUpdate");
+      socket.disconnect();
+    };
+  }, [currentUser.member_id, post]);
+
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+
+    const fetchUserPost = async () => {
+      try {
+        const res = await axios.get(`api/users/GETuser/${post.member_id}`);
+        const userData = res.data;
+        // console.log("User=", userData[0]);
+        setdataPUser(userData[0]);
+      } catch (err) {
+        // Handle error
+        console.error("Failed to fetch user data:", err);
+      }
+    };
+    const fetchComments = async () => {
+      try {
+        const resComments = await axios.get(
+          `/api/comments/${post.id}/Comments`
+        );
+        setComments(resComments.data);
+        setLoadingComment(true);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoadingComment(false);
+      }
+    };
+
+    fetchUserPost();
+    fetchComments();
+    handlePostUpdate(onPostUpdate);
+
+    return () => {
+      source.cancel("Component unmounted");
+    };
+  }, [post.member_id, post.id, showComments]);
+
+  // console.log("Comments=", comments);
+
+  //++++++++++ on Click Button +++++++++++
+
+  //Post----------------------------------------------------
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleEditPost = () => {
+    setdataEdit(post);
+    setdataEditID(post.id);
+    setTypeModal("Post");
+    setOpenModal(true);
+    handleClose();
+  };
+
+  const handleReportPost = () => {
+    setdataEdit(post);
+    setdataEdit("");
+    setOpenReportModal(true);
+    handleClose();
+  };
+
+  const handleReportUser = () => {
+    setdataEdit(post);
+    setdataEdit("");
+    setOpenReportModal(true);
+    handleClose();
+  };
+
+  const handlePostUpdate = (updatedPost, type) => {
+    if (type === "Post") {
+      // Handle updated post
+
+      post = { ...post, ...updatedPost };
+    } else if (type === "Comment") {
+      const updatedCommentIndex = comments.findIndex(
+        (comment) => comment.id === updatedPost.id
+      );
+
+      if (updatedCommentIndex === -1) {
+        // Comment is not present in the comments array, add it
+        setComments((prevComments) => [...prevComments, updatedPost]);
+      } else {
+        // Comment is already present, update it
+        setComments((prevComments) => {
+          const updatedComments = [...prevComments];
+          updatedComments[updatedCommentIndex] = updatedPost;
+          return updatedComments;
+        });
+      }
+    } else if (type === "Add Comment") {
+      // Handle added comment
+      setComments((prevComments) => [...prevComments, updatedPost]);
+    } else {
+      console.log("Invalid type: ", type);
+    }
+  };
+
+  const handleDeletePost = async () => {
+    const requestBody = {
+      member_id: currentUser.member_id,
+    };
+
+    try {
+      const response = await axios.delete(`/api/posts/${post.id}`, {
+        data: requestBody,
+      });
+      const message = response.data.message;
+      // Handle the response message here
+      console.log(message);
+      window.location.reload();
+      handleClose();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //ITEM OF POST ----------------------------------------------
+
+  const SortableItem = SortableElement(({ item, containerRef }) => {
+    const isImage = post.title === "image";
+
+    if (isImage) {
+      return (
+        <div className="shareImgItem">
+          <img src={item.url} alt="Gallery Image" className="shareImg" />
+        </div>
+      );
+    } else {
+      return (
+        <div className="shareVideoItem">
+          <ReactPlayer
+            url={item.url}
+            className="shareVideo"
+            width="640"
+            height="360"
+            controls
+            onClick={(event) => event.preventDefault()}
+          />
+        </div>
+      );
+    }
+  });
+
+  const SortableList = SortableContainer(({ items }) => {
+    return (
+      <div className="shareImgContainer" ref={containerRef}>
+        {items.map((item, index) => (
+          <SortableItem
+            key={index}
+            item={item}
+            index={index}
+            containerRef={containerRef}
+          />
+        ))}
+      </div>
+    );
+  });
+
+  //Comment-------------------------------------------
+
+  const handleClickComment = (event, id) => {
+    setAnchorElComment(event.currentTarget);
+    const comment = comments.find((c) => c.id === id);
+    // console.log(comment)
+    setdataEdit(comment);
+    setCommentIdUser(comment.memberId);
+    setCommentIdToDelete(id);
+  };
+
+  const handleCloseComment = () => {
+    setAnchorElComment(null);
+  };
+
+  const handleEditComment = () => {
+    const comment = comments.find((c) => c.id === commentIdToDelete);
+    console.log(comment);
+    setdataEdit(comment);
+    setdataEditID(commentIdToDelete);
+    setTypeModal("Comment");
+    setOpenModal(true);
+    handleCloseComment();
+  };
+
+  const handleDeleteComment = async () => {
+    console.log(`Delete Comment ${commentIdToDelete}`);
+    const requestBody = {
+      member_id: currentUser.member_id,
+    };
+
+    try {
+      const response = await axios.delete(
+        `/api/comments/${post.id}/comments/${commentIdToDelete}`,
+        { data: requestBody }
+      );
+      const message = response.data.message;
+      // Handle the response message here
+      console.log(message);
+
+      // Fetch the updated comments after deleting the comment
+      const resComments = await axios.get(`/api/comments/${post.id}/Comments`);
+      setComments(resComments.data);
+
+      handleClose();
+      setLoadingComment(true);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoadingComment(false);
+      handleClose();
+    }
+  };
+
+  const submitComment = () => {
+    setTypeModal("Add Comment");
+    setdataEdit("");
+    setIsAddComment(true);
+    setOpenModal(true);
+  };
+
+  //Like ------------------------------------------------
+
+  const likeHandler = async () => {
+    try {
+      // Send a PUT request to update the likes for the post
+      const res = await axios.put(`/api/posts/${post.id}/like`, {
+        member_id: currentUser.member_id,
+      });
+
+      if (isLiked) {
+        setLike(like - 1);
+      } else {
+        setLike(like + 1);
+      }
+      
+
+      // Emit a likePost event to the server
+      socket.emit("likePost", { postId: post.id, likes: res.data.likes });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="post" key={indexPost}>
+      <Card className="postWrapper">
+        <CardHeader
+          avatar={
+            <Link to={`/profile/${post.firstName}`}>
+              <Avatar
+                aria-label="recipe"
+                src={post?.profilePicture}
+                style={{ width: "39px", height: "39px" }}
+              ></Avatar>
+            </Link>
+          }
+          title={`${post?.firstName} ${post?.lastName}`}
+          subheader={formattedDate}
+          action={[
+            <IconButton key="more" onClick={handleClick}>
+              <MoreHoriz fontSize="small" />
+            </IconButton>,
+            <Menu
+              key="menu"
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              keepMounted
+            >
+              {dataPUser.member_id === user.member_id ? (
+                [
+                  <MenuItem key="edit" onClick={handleEditPost}>
+                    <span>
+                      <EditIcon fontSize="small" />
+                    </span>
+                    <span>Edit</span>
+                  </MenuItem>,
+                  <MenuItem key="delete" onClick={handleDeletePost}>
+                    <span>
+                      <DeleteIcon fontSize="small" />
+                    </span>
+                    <span>Delete</span>
+                  </MenuItem>,
+                ]
+              ) : (
+                <MenuItem key="report" onClick={handleReportPost}>
+                  <span>
+                    <ReportIcon fontSize="small" />
+                  </span>
+                  <span>Report Post</span>
+                </MenuItem>
+              )}
+            </Menu>,
+          ]}
+        />
+        <CardContent>
+          <Typography variant="body1" className="postText">
+            <span>{post?.content}</span>
+          </Typography>
+          <Typography variant="body2" className="postText">
+            {post?.tagpet.map((pet, i) => (
+              <span>
+                <Chip
+                  key={i}
+                  label={`#${pet}`}
+                  className="postChip"
+                  style={{ color: "#6200E8" }}
+                />
+              </span>
+            ))}
+          </Typography>
+          <SortableList
+            items={post.img.map((item) => ({
+              type: item.endsWith(".mp4") ? "video" : "image",
+              url: item,
+            }))}
+            axis="xy"
+          />
+        </CardContent>
+        <hr className="PostHr" />
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <CardActions disableSpacing>
+              {isLiked ? (
+                <IconButton aria-label="add to favorites" onClick={likeHandler}>
+                  <Favorite className="likeIcon" style={{ color: "#6200E8" }} />
+                </IconButton>
+              ) : (
+                <IconButton aria-label="add to favorites" onClick={likeHandler}>
+                  <FavoriteBorder
+                    className="likeIcon"
+                    style={{ color: "#6200E8" }}
+                  />
+                </IconButton>
+              )}
+              <span className="postLikeCounter">{like} people like it</span>
+            </CardActions>
+          </div>
+          <div className="postBottomRight">
+            <CardActions disableSpacing>
+              <IconButton
+                aria-label="show more"
+                sx={{ color: "#6200E8", right: 14 }}
+                onClick={() => setShowComments(!showComments)}
+              >
+                <Comment />
+                <p className="postCommentText" underline="none">
+                  <span>{comments.length} comments</span>
+                </p>
+              </IconButton>
+            </CardActions>
+          </div>
+        </div>
+        <Collapse in={showComments} timeout="auto" unmountOnExit>
+          <hr className="PostHr" />
+          {loadingComment ? (
+            <div className="loadingWrapper">
+              <ReactLoading
+                type="spin"
+                color="#6200E8"
+                height={"10%"}
+                width={"10%"}
+              />
+            </div>
+          ) : (
+            <CardContent>
+              <Typography paragraph>Comments</Typography>
+              {comments
+                .slice(
+                  0,
+                  showAllComments ? comments.length : maxDisplayedComments
+                )
+                .map((comment, index) => {
+                  return (
+                    <div key={index} className="postComment">
+                      <div className="postCommentProfile">
+                        <CardHeader
+                          avatar={
+                            <Avatar
+                              aria-label="recipe"
+                              src={comment?.profilePicture}
+                              sx={{ width: "39px", height: "39px" }}
+                            />
+                          }
+                          title={`${comment?.firstName} ${comment?.lastName}`}
+                          subheader={`${comment.content}`}
+                          action={
+                            <>
+                              <IconButton
+                                onClick={(event) =>
+                                  handleClickComment(event, comment.id)
+                                }
+                              >
+                                <MoreHoriz fontSize="small" />
+                              </IconButton>
+                            </>
+                          }
+                        />
+                        <Menu
+                          anchorEl={anchorElComment || undefined} // Provide undefined if anchorElComment is null
+                          open={Boolean(anchorElComment)}
+                          onClose={handleCloseComment}
+                        >
+                          {user.member_id === commentIdUser ? (
+                            [
+                              <MenuItem key="edit" onClick={handleEditComment}>
+                                <span>
+                                  <EditIcon fontSize="small" />
+                                </span>
+                                <span>Edit</span>
+                              </MenuItem>,
+                              <MenuItem
+                                key="delete"
+                                onClick={handleDeleteComment}
+                              >
+                                <span>
+                                  <DeleteIcon fontSize="small" />
+                                </span>
+                                <span>Delete</span>
+                              </MenuItem>,
+                            ]
+                          ) : (
+                            <MenuItem key="report" onClick={handleReportUser}>
+                              <span>
+                                <ReportIcon fontSize="small" />
+                              </span>
+                              <span>Report User</span>
+                            </MenuItem>
+                          )}
+                        </Menu>
+                      </div>
+                    </div>
+                  );
+                })}
+              {comments.length > maxDisplayedComments && (
+                <Typography
+                  className="showMoreComments"
+                  variant="body2"
+                  color="primary"
+                  onClick={() => setShowAllComments(!showAllComments)}
+                >
+                  {showAllComments ? "Hide" : "Show More"} Comments
+                </Typography>
+              )}
+              <Button variant="contained" onClick={() => submitComment()}>
+                Comment
+              </Button>
+            </CardContent>
+          )}
+        </Collapse>
+      </Card>
+      {openModal && (
+        <NestedModal
+          key={indexPost}
+          onClose={() => setOpenModal(false)}
+          onContent={dataEdit}
+          onTitle={typeModal}
+          userId={currentUser?.member_id}
+          onContentID={post?.id}
+          onCommentsID={dataEditID}
+          onLoading={true}
+          isAddComment={isAddComment}
+          onPostUpdate={(updatedComment) =>
+            handlePostUpdate(updatedComment, typeModal)
+          }
+        />
+      )}
+      {openReportModal && (
+        <ReportModal
+          key={indexPost}
+          onClose={() => setOpenReportModal(false)}
+          onContent={dataEdit}
+          onTitle={typeModal}
+          userId={currentUser?.member_id}
+          onContentID={post?.id}
+          onCommentsID={dataEditID}
+          onLoading={true}
+          onPostUpdate={handlePostUpdate}
+        />
+      )}
+    </div>
+  );
+}
