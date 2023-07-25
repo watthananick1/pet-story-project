d95e386d1c392f09d4d87199241dd3075015e529
nick <86664451+watthananick1@users.noreply.github.com>
2023-07-26 01:29:09 +0700

    Update all

---
+++ client/.history/src/apiCalls_20230726011441.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import {fetchUserData} from "./context/AuthContext";
+
+export const loginCall = async (userCredential, dispatch) => {
+  dispatch({ type: "LOGIN_START" });
+
+  try {
+    const res = await axios.post(`/api/auth/Login`, userCredential);
+    const user = res?.data;
+    const data = user?.userId;
+    const token = user?.token;
+
+    fetchUserData(data, token, dispatch);
+  } catch (err) {
+    // Dispatch LOGIN_FAILURE action with the error
+    dispatch({ type: "LOGIN_FAILURE", payload: err.response.data.error });
+  }
+};
